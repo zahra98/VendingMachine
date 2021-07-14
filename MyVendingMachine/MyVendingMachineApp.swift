9419857f4157608d777d72f945fd3d35081fc925
zahra98 <zahraabuzahra4@gmail.com>
2021-07-14 10:07:20 +0300

    Initial Commit

---
+++ MyVendingMachine/MyVendingMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVendingMachineApp.swift
+//  MyVendingMachine
+//
+//  Created by mac on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyVendingMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
